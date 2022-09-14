9a3bdcb73af00140ca3da31cf92973338565c5e4
BesttJTL <111588284+BesttJTL@users.noreply.github.com>
2022-09-14 21:53:13 +0700

    Finish

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,22 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
+  if (array1==undefined && array1==null && array2==undefined && array2==null){
+    console.log(undefined)
+    return undefined
+}
+else if(array1==undefined || array1==null  && array2==true){
+    console.log(array2)
+    return array2
+}
+else if(array2==undefined || array2==null  && array1==true){
+        console.log(array1)
+        return array1
+}
+else {
+    let concatArrays = array1.concat(array2)
+  console.log(concatArrays)
+  return concatArrays
+  }
 }
 module.exports = concatArray
